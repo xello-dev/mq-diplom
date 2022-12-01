ae03c14e390562af937cf8190645eefe7572d3e4
Sergey Balashov <68418454+xello-dev@users.noreply.github.com>
2022-12-01 13:36:31 +0400

    123
    123

--- site/js/main.js
+++ site/js/main.js
@@ -1,3 +1,15 @@
-$(document).scroll(function(e) {
-    $(window).scrollTop() > 100 ? $('.nav').addClass('nav__color') : $('.nav').removeClass('nav__color');
+var number = document.querySelector('.count'),
+numberTop = number.getBoundingClientRect().top,
+start = +number.innerHTML, end = +number.dataset.max;
+
+window.addEventListener('scroll', function onScroll() {
+    if(window.pageYOffset > numberTop - window.innerHeight / 2) {
+        this.removeEventListener('scroll', onScroll);
+        var interval = setInterval(function() {
+            number.innerHTML = ++start;
+            if(start == end) {
+                clearInterval(interval);
+            }
+        }, 5);
+    }
 });
